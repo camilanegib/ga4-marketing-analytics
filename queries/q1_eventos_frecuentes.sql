6a3087f8e69a9b571e2114c5b015547f1d2c2b6d
camilanegib <camilanegib@gmail.com>
2026-06-25 17:34:10 -0500

    Delete queries directory

--- queries/q1_eventos_frecuentes.sql
+++
@@ -1,8 +0,0 @@
-SELECT
-  event_name,
-  COUNT(*) AS numero_eventos
-FROM `bigquery-public-data.ga4_obfuscated_sample_ecommerce.events_*`
-WHERE _TABLE_SUFFIX BETWEEN '20201101' AND '20210131'
-GROUP BY event_name
-ORDER BY numero_eventos DESC
-LIMIT 10
--- queries/queries/q2_usuarios_unicos_por_fecha.sql
+++
@@ -1,7 +0,0 @@
-SELECT
-  event_date,
-  COUNT(DISTINCT user_pseudo_id) AS usuarios_unicos
-FROM `bigquery-public-data.ga4_obfuscated_sample_ecommerce.events_*`
-WHERE _TABLE_SUFFIX BETWEEN '20201101' AND '20210131'
-GROUP BY event_date
-ORDER BY event_date ASC
